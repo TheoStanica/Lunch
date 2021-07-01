a243a356fc8b70cdc44c0831036199760fa9efc9
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2021-07-01 16:53:49 +0300

    created validation for user

---
+++ Backend/src/middleware/bodyValidation.js
@@ -0,0 +1,43 @@
+const RequestValidationError = require('../errors/requestValidationError');
+const { check, validationResult } = require('express-validator');
+
+const validationResults = (req, res, next) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    throw new RequestValidationError(errors.array());
+  }
+  next();
+};
+
+const isUserValid = [
+  check('email')
+    .notEmpty()
+    .withMessage('Email is required')
+    .normalizeEmail()
+    .isEmail()
+    .withMessage('Invalid email'),
+  check('password')
+    .notEmpty()
+    .withMessage('Password is required.')
+    .isLength({ min: 8 })
+    .withMessage('Password must have a minimum length of 8.')
+    .matches(/([A-Z])+/)
+    .withMessage('Password must have at least one upper letter.')
+    .matches(/[a-z]+/)
+    .withMessage('Password must have at least one lower letter.')
+    .matches(/\d+/)
+    .withMessage('Password must have at least one digit.')
+    .matches(/[@$!%*?&]+/)
+    .withMessage('Password must have at least one special character.'),
+  check('fullname')
+    .notEmpty()
+    .withMessage('Name is required.')
+    .matches(/^[A-Z][-/sa-zA-Z]+$/)
+    .withMessage('First name should contain only letters.'),
+];
+
+module.exports = {
+  validationResults,
+  isUserValid,
+};
