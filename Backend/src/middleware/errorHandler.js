61c93ad22456dba38e29db1a362abb0ac5b2cfa9
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2021-07-01 16:52:20 +0300

    created an error handler middleware

---
+++ Backend/src/middleware/errorHandler.js
@@ -0,0 +1,13 @@
+const CustomError = require('../errors/customError');
+
+const errorHandler = (err, req, res, next) => {
+  if (err instanceof CustomError) {
+    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
+  }
+
+  res.status(400).send({
+    errors: [{ message: 'Something went wrong' }],
+  });
+};
+
+module.exports = errorHandler;
