b0ea2e35988fc86f933f4e8e260a07c8bdf2b90f
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2021-07-01 16:54:36 +0300

    created the user model

---
+++ Backend/src/user/model.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ['user', 'admin'],
+      default: 'user',
+    },
+    status: {
+      type: String,
+      enum: ['pending', 'active'],
+      default: 'pending',
+    },
+    activationToken: {
+      type: String,
+      uniqe: true,
+    },
+  },
+  {
+    timestamps: {
+      createdAt: true,
+    },
+  }
+);
+
+userSchema.options.toJSON = {
+  getters: true,
+  virtuals: true,
+  minimize: false,
+  transform: function (doc, ret, options) {
+    delete ret.password;
+    return ret;
+  },
+};
+
+module.exports = mongoose.model('User', userSchema);
