225e8d86f493957e3b09881c3b7e35d09c345f5e
Ovidiu Firescu <ovidiufirescu@gmail.com>
2021-07-02 15:02:02 +0300

    added template for forgot password email

--- Backend/src/utils/emailTemplates.js
+++ Backend/src/utils/emailTemplates.js
@@ -3,12 +3,22 @@ const Transporter = require('./transporter');
 const sendActivationEmail = (user) => {
   Transporter.sentEmail({
     to: user.email,
-    subject: 'Account Activation',
+    subject: 'Lunch App Account Activation',
     html: `<h2>Hello ${user.fullname}</h2>
       <p>Please confirm your email by clicking on the following link</p>
       <a href=http://${process.env.HOST_URL}/api/user/activate/${user.activationToken}>Click here</a>
-      </div>`,
+      `,
   });
 };
 
-module.exports = { sendActivationEmail };
+const sendForgotPasswordEmail = (user) => {
+  Transporter.sentEmail({
+    to: user.email,
+    subject: 'Lunch App Forgot Password',
+    html: `<h2>Hello ${user.fullname}</h2>
+          <p>Please enter the following ${user.forgotPasswordToken} into the application to reset your password.</p>
+          `,
+  });
+};
+
+module.exports = { sendActivationEmail, sendForgotPasswordEmail };
