dab4fe4ed48d62cb6c092f39b0bbb6c5d7846730
Ovidiu Firescu <ovidiufirescu@gmail.com>
2021-07-02 11:16:21 +0300

    Created email templates to send emails based on needs

---
+++ Backend/src/utils/emailTemplates.js
@@ -0,0 +1,15 @@
+const Transporter = require("./transporter");
+
+const sendActivationEmail = (user) => {
+  Transporter.sentEmail({
+    //to: user.email,
+    to: "ovidiuoviovi174@gmail.com",
+    subject: "Account Activation",
+    html: `<h2>Hello ${user.fullname}</h2>
+      <p>Please confirm your email by clicking on the following link</p>
+      <a href=http://192.168.100.2:3000/api/user/confirm/${user.activationToken}>Click here</a>
+      </div>`,
+  });
+};
+
+module.exports = { sendActivationEmail };
