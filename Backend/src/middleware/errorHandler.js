41b58e0d781a6c771cc9cceec38b7815e3db206f
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2021-07-02 14:08:47 +0300

    added console log

--- Backend/src/middleware/errorHandler.js
+++ Backend/src/middleware/errorHandler.js
@@ -4,6 +4,7 @@ const errorHandler = (err, req, res, next) => {
   if (err instanceof CustomError) {
     return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
+  console.log(err);
 
   res.status(400).send({
     errors: [{ message: 'Something went wrong' }],
