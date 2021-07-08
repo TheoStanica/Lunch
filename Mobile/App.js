99bb90e45ae82a4d318d8c91ec462cb0faf51ff9
TheoStanica <theodor.stanica@gmail.com>
2021-07-08 10:52:00 +0300

    added redux persistance

--- Mobile/App.js
+++ Mobile/App.js
@@ -1,19 +1,21 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import store from './src/redux/store';
+import {store, persistor} from './src/redux/store';
 import AppNavigatorRoutes from './src/navigation/stackNavigationRoutes';
-
 import {SafeAreaView, StyleSheet} from 'react-native';
 import {Provider} from 'react-redux';
 import {NavigationContainer} from '@react-navigation/native';
+import {PersistGate} from 'redux-persist/integration/react';
 
 const App = () => {
   return (
     <SafeAreaView style={styles.container}>
       <Provider store={store}>
-        <NavigationContainer>
-          <AppNavigatorRoutes />
-        </NavigationContainer>
+        <PersistGate loading={null} persistor={persistor}>
+          <NavigationContainer>
+            <AppNavigatorRoutes />
+          </NavigationContainer>
+        </PersistGate>
       </Provider>
     </SafeAreaView>
   );
--- Mobile/src/redux/store.js
+++ Mobile/src/redux/store.js
@@ -2,15 +2,23 @@ import {createStore, applyMiddleware} from 'redux';
 import {composeWithDevTools} from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import combineReducers from './reducers';
-
-const initialState = {};
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {persistStore, persistReducer} from 'redux-persist';
 
 const middleware = [thunk];
 
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+  whitelist: ['userReducer'],
+};
+
+const pReducer = persistReducer(persistConfig, combineReducers);
+
 const store = createStore(
-  combineReducers,
-  initialState,
+  pReducer,
   composeWithDevTools(applyMiddleware(...middleware)),
 );
+let persistor = persistStore(store);
 
-export default store;
+export {store, persistor};
