06464e3d82df19686e21fd0671caefb31845da8c
Ovidiu Firescu <ovidiufirescu@gmail.com>
2021-07-09 16:16:33 +0300

    finished forgot password screen

--- Mobile/src/screens/forgotPasswordScreen.js
+++ Mobile/src/screens/forgotPasswordScreen.js
@@ -6,80 +6,97 @@ import {
   emailValidationSchema,
   passwordValidationSchema,
 } from '../assets/bodyValidation/userValidation';
+import {useDispatch, useSelector} from 'react-redux';
 
 import TextInputField from '../components/textInputField';
+import {forgotPasswordUser} from '../redux/thunks/userThunks';
 
-const ForgotPasswordScreen = ({theme}) => {
+const ForgotPasswordScreen = ({theme, route}) => {
+  const {message} = useSelector(state => state.userReducer);
+  const token = route?.params?._token;
+  const dispatch = useDispatch();
+
   return (
     <SafeAreaView style={styles.container(theme)}>
       <View style={styles.contentContainer}>
-        <Formik
-          validationSchema={emailValidationSchema}
-          initialValues={{
-            email: '',
-          }}
-          onSubmit={values => dispatch(loginUser({email: values.email}))}>
-          {({values, handleChange, errors, isValid, handleSubmit}) => (
-            <>
-              <TextInputField
-                label="Email"
-                value={values.email}
-                errors={errors.email}
-                handleChange={handleChange}
-                field="email"
-              />
-              <Button
-                mode="contained"
-                disabled={!isValid}
-                onPress={handleSubmit}
-                style={styles.glassButton}
-                color="#fff7">
-                <Text style={styles.buttonText}>Send Email</Text>
-              </Button>
-            </>
-          )}
-        </Formik>
-        <Formik
-          validationSchema={passwordValidationSchema}
-          initialValues={{
-            password: '',
-            retypePassword: '',
-          }}
-          onSubmit={values =>
-            dispatch(
-              loginUser({
-                password: values.password,
-                retypePassword: values.retypePassword,
-              }),
-            )
-          }>
-          {({values, handleChange, errors, isValid, handleSubmit}) => (
-            <>
-              <TextInputField
-                label="New Password"
-                value={values.password}
-                errors={errors.password}
-                handleChange={handleChange}
-                field="password"
-              />
-              <TextInputField
-                label="Retype New Password"
-                value={values.retypePassword}
-                errors={errors.retypePassword}
-                handleChange={handleChange}
-                field="retypePassword"
-              />
-              <Button
-                mode="contained"
-                disabled={!isValid}
-                onPress={handleSubmit}
-                style={styles.glassButton}
-                color="#fff7">
-                <Text style={styles.buttonText}>Reset Password</Text>
-              </Button>
-            </>
-          )}
-        </Formik>
+        {!token ? (
+          <Formik
+            validationSchema={emailValidationSchema}
+            initialValues={{
+              email: '',
+            }}
+            onSubmit={values => {
+              dispatch(forgotPasswordUser({email: values.email}));
+              values.email = '';
+            }}>
+            {({values, handleChange, errors, isValid, handleSubmit}) => (
+              <>
+                <TextInputField
+                  label="Email"
+                  value={values.email}
+                  errors={errors.email}
+                  handleChange={handleChange}
+                  field="email"
+                />
+                <Button
+                  mode="contained"
+                  disabled={!isValid}
+                  onPress={handleSubmit}
+                  style={styles.glassButton}
+                  color="#fff7">
+                  <Text style={styles.buttonText}>Send Email</Text>
+                </Button>
+              </>
+            )}
+          </Formik>
+        ) : (
+          <Formik
+            validationSchema={passwordValidationSchema}
+            initialValues={{
+              password: '',
+              retypePassword: '',
+            }}
+            onSubmit={values =>
+              dispatch(
+                forgotPasswordUser({
+                  password: values.password,
+                  token,
+                }),
+              )
+            }>
+            {({values, handleChange, errors, isValid, handleSubmit}) => (
+              <>
+                <TextInputField
+                  label="New Password"
+                  value={values.password}
+                  errors={errors.password}
+                  handleChange={handleChange}
+                  secureTextEntry={true}
+                  field="password"
+                />
+                <TextInputField
+                  label="Retype New Password"
+                  value={values.retypePassword}
+                  errors={errors.retypePassword}
+                  handleChange={handleChange}
+                  secureTextEntry={true}
+                  field="retypePassword"
+                />
+                <Button
+                  mode="contained"
+                  disabled={!isValid}
+                  onPress={handleSubmit}
+                  style={styles.glassButton}
+                  color="#fff7">
+                  <Text style={styles.buttonText}>Reset Password</Text>
+                </Button>
+                {message ? (
+                  <Text style={styles.successMessage}>{message}</Text>
+                ) : null}
+              </>
+            )}
+          </Formik>
+        )}
       </View>
     </SafeAreaView>
   );
