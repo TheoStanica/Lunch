9fe58a0bc456c93b74b2962e200d2d2a845b02da
Ovidiu Firescu <ovidiufirescu@gmail.com>
2021-07-09 11:16:09 +0300

    moved user validation schemas into a separate folder

--- Mobile/src/screens/registerScreen.js
+++ Mobile/src/screens/registerScreen.js
@@ -4,38 +4,12 @@ import {Button} from 'react-native-paper';
 import {useDispatch} from 'react-redux';
 import {registerUser} from '../redux/thunks/userThunks';
 import {Formik} from 'formik';
-import * as yup from 'yup';
+import {registerValidationSchema} from '../assets/bodyValidation/userValidation';
 
 import TextInputField from '../components/textInputField';
 
 const RegisterScreen = () => {
   const dispatch = useDispatch();
-
-  const registerValidationSchema = yup.object({
-    email: yup
-      .string('Enter your email.')
-      .email('Enter a valid email.')
-      .required('Email is required.'),
-    password: yup
-      .string('Enter your password.')
-      .min(8, 'Password must have a minimum length of 8.')
-      .matches(/([A-Z])+/, 'Password must have at least one upper letter.')
-      .matches(/[a-z]+/, 'Password must have at least one lower letter.')
-      .matches(/\d+/, 'Password must have at least one digit.')
-      .matches(
-        /[@$!%*?&]+/,
-        'Password must have at least one special character.',
-      )
-      .required('Password is required.'),
-    retypePassword: yup
-      .string('Retype your password.')
-      .oneOf([yup.ref('password')], 'Passwords must match')
-      .required('Retype password is required.'),
-    fullname: yup
-      .string('Enter your full name.')
-      .matches(/^[A-Z][- a-zA-Z]+$/, 'Enter a valid full name.')
-      .required('Full name is required.'),
-  });
 
   return (
     <SafeAreaView style={styles.container}>
