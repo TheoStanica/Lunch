55632aeae20320d29892c6403eaddf2e9ddbd3cb
TheoStanica <theodor.stanica@gmail.com>
2021-07-09 11:37:27 +0300

    fixed conflicts

--- Mobile/src/screens/loginScreen.js
+++ Mobile/src/screens/loginScreen.js
@@ -5,14 +5,15 @@ import {
   Text,
   KeyboardAvoidingView,
   View,
-  Keyboard,
-  TouchableWithoutFeedback,
+  ScrollView,
+  Platform,
 } from 'react-native';
 import {Button, withTheme} from 'react-native-paper';
 import {useDispatch} from 'react-redux';
 import {loginUser} from '../redux/thunks/userThunks';
 import {Formik} from 'formik';
 import {loginValidationSchema} from '../assets/bodyValidation/userValidation';
+import HideKeyboard from '../components/hideKeyboard';
 
 import TextInputField from '../components/textInputField';
 
@@ -20,63 +21,66 @@ const LoginScreen = ({navigation, theme}) => {
   const dispatch = useDispatch();
 
   return (
-    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+    <HideKeyboard>
       <SafeAreaView style={styles.container(theme)}>
-        <KeyboardAvoidingView behavior="padding">
-          <View style={styles.contentContainer}>
-            <Formik
-              validationSchema={loginValidationSchema}
-              initialValues={{
-                email: '',
-                password: '',
-              }}
-              onSubmit={values =>
-                dispatch(
-                  loginUser({email: values.email, password: values.password}),
-                )
-              }>
-              {({values, handleChange, errors, isValid, handleSubmit}) => (
-                <>
-                  <TextInputField
-                    label="Email"
-                    value={values.email}
-                    errors={errors.email}
-                    handleChange={handleChange}
-                    field="email"
-                  />
-                  <TextInputField
-                    label="Password"
-                    value={values.password}
-                    errors={errors.password}
-                    handleChange={handleChange}
-                    secureTextEntry={true}
-                    field="password"
-                  />
-                  <SafeAreaView>
-                    <Button
-                      mode="contained"
-                      disabled={!isValid}
-                      onPress={handleSubmit}
-                      style={styles.glassButton}
-                      color="#fff7">
-                      <Text style={styles.buttonText}>Login</Text>
-                    </Button>
-                  </SafeAreaView>
-                </>
-              )}
-            </Formik>
-            <SafeAreaView>
-              <Button
-                onPress={() => {
-                  navigation.navigate('ForgotPasswordScreen');
-                }}>
-                <Text style={styles.buttonText}>Forgot Password</Text>
-              </Button>
-            </SafeAreaView>
-          </View>
+        <KeyboardAvoidingView
+          behavior={Platform.OS === 'ios' ? 'padding' : 'margin'}>
+          <ScrollView>
+            <View style={styles.contentContainer}>
+              <Formik
+                validationSchema={loginValidationSchema}
+                initialValues={{
+                  email: '',
+                  password: '',
+                }}
+                onSubmit={values =>
+                  dispatch(
+                    loginUser({email: values.email, password: values.password}),
+                  )
+                }>
+                {({values, handleChange, errors, isValid, handleSubmit}) => (
+                  <>
+                    <TextInputField
+                      label="Email"
+                      value={values.email}
+                      errors={errors.email}
+                      handleChange={handleChange}
+                      field="email"
+                    />
+                    <TextInputField
+                      label="Password"
+                      value={values.password}
+                      errors={errors.password}
+                      handleChange={handleChange}
+                      secureTextEntry={true}
+                      field="password"
+                    />
+                    <SafeAreaView>
+                      <Button
+                        mode="contained"
+                        disabled={!isValid}
+                        onPress={handleSubmit}
+                        style={styles.glassButton}
+                        color="#fff7">
+                        <Text style={styles.buttonText}>Login</Text>
+                      </Button>
+                    </SafeAreaView>
+                  </>
+                )}
+              </Formik>
+              <SafeAreaView>
+                <Button
+                  onPress={() => {
+                    navigation.navigate('ForgotPasswordScreen');
+                  }}>
+                  <Text style={styles.buttonText}>Forgot Password</Text>
+                </Button>
+              </SafeAreaView>
+            </View>
+          </ScrollView>
         </KeyboardAvoidingView>
       </SafeAreaView>
-    </TouchableWithoutFeedback>
+    </HideKeyboard>
   );
 };
 
