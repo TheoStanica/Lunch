210dda6e9bf3aca18b6a4296ccd770ee9c6088a0
Ovidiu Firescu <ovidiufirescu@gmail.com>
2021-07-12 11:52:20 +0300

    removed unnecessary code

--- Backend/src/utils/emailTemplates.js
+++ Backend/src/utils/emailTemplates.js
@@ -7,7 +7,6 @@ const sendActivationEmail = (user) => {
     html: `<h2>Hello ${user.fullname}</h2>
       <p>Please confirm your email by clicking on the following link</p>
       <a href=http://${process.env.HOST_URL}/api/user/activate/${user.activationToken}>Click here</a>
-      <a href=lunchapp://forgotpassword/${user.activationToken}>Test</a>
       `,
   });
 };
