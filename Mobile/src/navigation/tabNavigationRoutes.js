76d8f69ea3cc7851a1a33b2feb6345222ab6d27e
Ovidiu Firescu <ovidiufirescu@gmail.com>
2021-07-12 15:04:35 +0300

    Added icon and label for tab bars

--- Mobile/src/navigation/tabNavigationRoutes.js
+++ Mobile/src/navigation/tabNavigationRoutes.js
@@ -3,13 +3,31 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/homeScreen';
 import ProfileScreen from '../screens/profileScreen';
 
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
 const Tab = createBottomTabNavigator();
 
 const TabNavigatorRoutes = () => {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="HomeScreen" component={HomeScreen} />
-      <Tab.Screen name="ProfileScreen" component={ProfileScreen} />
+      <Tab.Screen
+        name="HomeScreen"
+        component={HomeScreen}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({color}) => <Icon name="home" size={25} color={color} />,
+        }}
+      />
+      <Tab.Screen
+        name="ProfileScreen"
+        component={ProfileScreen}
+        options={{
+          tabBarLabel: 'Profile',
+          tabBarIcon: ({color}) => (
+            <Icon name="account" size={25} color={color} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
