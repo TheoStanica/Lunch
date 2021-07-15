87d2badaf1b130c68d3d5b645eebbbdfcecc23ee
TheoStanica <theodor.stanica@gmail.com>
2021-07-15 17:46:55 +0300

    display the list of restaurants on screen

--- Mobile/src/screens/manageRestaurantsScreen.js
+++ Mobile/src/screens/manageRestaurantsScreen.js
@@ -1,10 +1,64 @@
-import React from 'react';
-import {SafeAreaView, Text, StyleSheet} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {SafeAreaView, Text, StyleSheet, FlatList, View} from 'react-native';
+import {Modal} from 'react-native-paper';
+import {useDispatch, useSelector} from 'react-redux';
+import ActionButton from '../components/actionButton';
+import AdminField from '../components/adminField';
+import {getRestaurants} from '../redux/thunks/restaurantThunks';
 
 const ManageRestaurantsScreen = () => {
+  const {restaurants, restaurantsById} = useSelector(
+    state => state.restaurantReducer,
+  );
+  const [selectedRestaurant, setSelectedRestaurant] = useState(null);
+  const [isFetching, setIsFetching] = useState(false);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getRestaurants({onFinish: () => {}}));
+  }, []);
+
+  const onRefresh = () => {
+    console.log('refreshing');
+    setIsFetching(true);
+    dispatch(
+      getRestaurants({
+        onFinish: () => setIsFetching(false),
+      }),
+    );
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <Text>Manage Restaurants</Text>
+      <FlatList
+        data={restaurants}
+        keyExtractor={restaurant => restaurant}
+        renderItem={restaurant => (
+          <AdminField
+            id={restaurant.item}
+            title={restaurantsById[restaurant.item].name}
+            description={restaurantsById[restaurant.item].cost}
+            icon="food"
+            onPress={() =>
+              setSelectedRestaurant(restaurantsById[restaurant.item])
+            }
+          />
+        )}
+        onRefresh={onRefresh}
+        refreshing={isFetching}
+      />
+      <Modal
+        visible={selectedRestaurant}
+        onDismiss={() => setSelectedRestaurant(null)}>
+        <View style={{backgroundColor: 'white'}}>
+          <Text>Restaurant info {selectedRestaurant?.name}</Text>
+          <View style={{flexDirection: 'row'}}>
+            <ActionButton text="Ceva" style={{flex: 1}}></ActionButton>
+            <ActionButton text="Altceva" style={{flex: 1}}></ActionButton>
+          </View>
+        </View>
+      </Modal>
     </SafeAreaView>
   );
 };
