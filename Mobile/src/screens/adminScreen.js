801127fb12721638557c79f3c714e7fe8da700c9
Ovidiu Firescu <ovidiufirescu@gmail.com>
2021-07-15 13:37:45 +0300

    added in admin screen buttons to manage users and manage restaurants

--- Mobile/src/screens/adminScreen.js
+++ Mobile/src/screens/adminScreen.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import {SafeAreaView, Text, StyleSheet} from 'react-native';
+import {SafeAreaView, View, StyleSheet} from 'react-native';
 
-const AdminScreen = () => {
+import ActionButton from '../components/actionButton';
+
+const AdminScreen = ({navigation}) => {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Admin</Text>
+      <View style={styles.contentContainer}>
+        <ActionButton
+          text="Manage users"
+          onPress={() => navigation.navigate('ManageUsersScreen')}
+        />
+        <ActionButton
+          text="Manage restaurants"
+          onPress={() => navigation.navigate('ManageRestaurantsScreen')}
+        />
+      </View>
     </SafeAreaView>
   );
 };
@@ -12,6 +23,10 @@ const AdminScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  contentContainer: {
+    flex: 1,
+    padding: 25,
   },
 });
 
