099e3cd8e71694326ffa0de2d2b7b01278e93466
TheoStanica <theodor.stanica@gmail.com>
2021-07-15 14:46:15 +0300

    created adminField component

---
+++ Mobile/src/components/adminField.js
@@ -0,0 +1,40 @@
+import {TouchableRipple, List} from 'react-native-paper';
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
+const AdminField = ({onPress, title, description, icon, iconColor}) => {
+  return (
+    <TouchableRipple onPress={onPress} rippleColor="#4A6572">
+      <List.Item
+        title={title}
+        titleStyle={styles.title}
+        description={description}
+        descriptionStyle={styles.description}
+        left={() =>
+          icon ? (
+            <View style={styles.icon}>
+              <Icon size={40} name={icon} color={iconColor} />
+            </View>
+          ) : null
+        }
+      />
+    </TouchableRipple>
+  );
+};
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 18,
+    textTransform: 'capitalize',
+  },
+  description: {
+    textTransform: 'capitalize',
+  },
+  icon: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default AdminField;
