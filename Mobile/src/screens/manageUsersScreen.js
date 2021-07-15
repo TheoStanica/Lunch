c785809fd349bd253841aa99efe475b7cd76a783
Ovidiu Firescu <ovidiufirescu@gmail.com>
2021-07-15 16:04:27 +0300

    implemented manage users screen and other minor changes

--- Mobile/src/screens/manageUsersScreen.js
+++ Mobile/src/screens/manageUsersScreen.js
@@ -1,10 +1,41 @@
-import React from 'react';
-import {SafeAreaView, Text, StyleSheet} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {SafeAreaView, StyleSheet, FlatList} from 'react-native';
+import {getAllUsers} from '../redux/thunks/userThunks';
+import {useDispatch} from 'react-redux';
+
+import AdminField from '../components/adminField';
 
 const ManageUsersScreen = () => {
+  const [users, setUsers] = useState([]);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(
+      getAllUsers({
+        onFinish: users => {
+          if (users) {
+            setUsers(users);
+          }
+        },
+      }),
+    );
+  }, []);
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Manage Users</Text>
+      <FlatList
+        data={users}
+        keyExtractor={item => item.id}
+        renderItem={({item}) => {
+          return (
+            <AdminField
+              title={item.fullname}
+              description={item.email}
+              icon="account-edit"
+            />
+          );
+        }}
+      />
     </SafeAreaView>
   );
 };
