18dfaf6abab39198a7ee3e40904cdbdb62ad7cfb
TheoStanica <theodor.stanica@gmail.com>
2021-07-19 16:03:44 +0300

    Added FAB button to create new restaurants, changed useEffect for useFocusEffect

--- Mobile/src/screens/manageRestaurantsScreen.js
+++ Mobile/src/screens/manageRestaurantsScreen.js
@@ -1,5 +1,6 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState, useCallback} from 'react';
 import {StyleSheet, FlatList} from 'react-native';
+import {FAB} from 'react-native-paper';
 import {useDispatch, useSelector} from 'react-redux';
 import AdminField from '../components/adminField';
 import {
@@ -7,6 +8,7 @@ import {
   getRestaurants,
 } from '../redux/thunks/restaurantThunks';
 import HideKeyboard from '../components/hideKeyboard';
+import {useFocusEffect} from '@react-navigation/native';
 
 const ManageRestaurantsScreen = ({navigation}) => {
   const {restaurants, restaurantsById} = useSelector(
@@ -15,43 +17,58 @@ const ManageRestaurantsScreen = ({navigation}) => {
   const [isFetching, setIsFetching] = useState(true);
   const dispatch = useDispatch();
 
-  useEffect(() => {
-    dispatch(
-      getRestaurants(() => {
-        setIsFetching(false);
-      }),
-    );
-  }, []);
-
   const onRefresh = () => {
     setIsFetching(true);
     dispatch(getRestaurants(() => setIsFetching(false)));
   };
+  useFocusEffect(
+    useCallback(() => {
+      onRefresh();
+    }, []),
+  );
 
   return (
     <HideKeyboard>
-      <FlatList
-        data={restaurants}
-        keyExtractor={restaurant => restaurant}
-        renderItem={restaurant => (
-          <AdminField
-            id={restaurant.item}
-            title={restaurantsById[restaurant.item].name}
-            description={`Cost: ${restaurantsById[restaurant.item].cost} lei`}
-            icon="food"
-            onDelete={() => dispatch(deleteRestaurant({id: restaurant.item}))}
-            onEdit={() =>
-              navigation.navigate('RestaurantDetailsScreen', {
-                restaurantId: restaurant.item,
-              })
-            }
-          />
-        )}
-        onRefresh={onRefresh}
-        refreshing={isFetching}
-      />
+      <>
+        <FlatList
+          data={restaurants}
+          keyExtractor={restaurant => restaurant}
+          renderItem={restaurant => (
+            <AdminField
+              id={restaurant.item}
+              title={restaurantsById[restaurant.item].name}
+              description={`Cost: ${restaurantsById[restaurant.item].cost} lei`}
+              icon="food"
+              onDelete={() => dispatch(deleteRestaurant({id: restaurant.item}))}
+              onEdit={() =>
+                navigation.navigate('RestaurantDetailsScreen', {
+                  restaurantId: restaurant.item,
+                })
+              }
+            />
+          )}
+          onRefresh={onRefresh}
+          refreshing={isFetching}
+        />
+        <FAB
+          style={styles.fab}
+          icon="plus"
+          color="white"
+          animated={true}
+          onPress={() => navigation.navigate('CreateRestaurantScreen')}
+        />
+      </>
     </HideKeyboard>
   );
 };
+
+const styles = StyleSheet.create({
+  fab: {
+    position: 'absolute',
+    margin: 20,
+    right: 0,
+    bottom: 0,
+  },
+});
 
 export default ManageRestaurantsScreen;
