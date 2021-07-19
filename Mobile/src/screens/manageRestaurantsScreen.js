8a5c6f3d16eaebb73795af5a7beea4f9ea638304
TheoStanica <theodor.stanica@gmail.com>
2021-07-19 14:05:03 +0300

    updated manageRestaurantsScreen

--- Mobile/src/screens/manageRestaurantsScreen.js
+++ Mobile/src/screens/manageRestaurantsScreen.js
@@ -1,134 +1,57 @@
 import React, {useEffect, useState} from 'react';
-import {SafeAreaView, Text, StyleSheet, FlatList, View} from 'react-native';
-import {Modal} from 'react-native-paper';
+import {StyleSheet, FlatList} from 'react-native';
 import {useDispatch, useSelector} from 'react-redux';
-import ActionButton from '../components/actionButton';
 import AdminField from '../components/adminField';
-import {getRestaurants} from '../redux/thunks/restaurantThunks';
-import {Formik} from 'formik';
-import TextInputField from '../components/textInputField';
-import DropDownPicker from 'react-native-dropdown-picker';
+import {
+  deleteRestaurant,
+  getRestaurants,
+} from '../redux/thunks/restaurantThunks';
 import HideKeyboard from '../components/hideKeyboard';
 
-const ManageRestaurantsScreen = () => {
+const ManageRestaurantsScreen = ({navigation}) => {
   const {restaurants, restaurantsById} = useSelector(
     state => state.restaurantReducer,
   );
-  const [selectedRestaurant, setSelectedRestaurant] = useState(null);
-  const [isFetching, setIsFetching] = useState(false);
-  const [dropdownVIsible, setDropdownVisible] = useState(false);
-  const [value, setValue] = useState(null);
+  const [isFetching, setIsFetching] = useState(true);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(getRestaurants({onFinish: () => {}}));
+    dispatch(
+      getRestaurants(() => {
+        setIsFetching(false);
+      }),
+    );
   }, []);
 
   const onRefresh = () => {
     setIsFetching(true);
-    dispatch(
-      getRestaurants({
-        onFinish: () => setIsFetching(false),
-      }),
-    );
+    dispatch(getRestaurants(() => setIsFetching(false)));
   };
 
   return (
     <HideKeyboard>
-      <SafeAreaView style={styles.container}>
-        <FlatList
-          data={restaurants}
-          keyExtractor={restaurant => restaurant}
-          renderItem={restaurant => (
-            <AdminField
-              id={restaurant.item}
-              title={restaurantsById[restaurant.item].name}
-              description={restaurantsById[restaurant.item].cost}
-              icon="food"
-              onPress={() => {
-                setSelectedRestaurant(restaurantsById[restaurant.item]);
-                setValue(restaurantsById[restaurant.item].status);
-              }}
-            />
-          )}
-          onRefresh={onRefresh}
-          refreshing={isFetching}
-        />
-        {/* {selectedRestaurant ? ( */}
-        <Modal
-          visible={selectedRestaurant}
-          onDismiss={() => {
-            setSelectedRestaurant(null);
-            setValue(null);
-            setDropdownVisible(false);
-          }}
-          contentContainerStyle={{
-            padding: 20,
-            marginHorizontal: 20,
-            backgroundColor: 'white',
-          }}>
-          <View>
-            <Text style={styles.title}>Update Restaurant</Text>
-            <Formik
-              initialValues={{
-                name: selectedRestaurant?.name,
-                cost: `${selectedRestaurant?.cost}`,
-                status: selectedRestaurant?.status,
-              }}
-              onSubmit={values => {
-                values.status = value;
-                console.log('submitting form with ', values);
-              }}>
-              {({values, handleChange, errors, handleSubmit}) => (
-                <>
-                  <TextInputField
-                    label="Name"
-                    value={values.name}
-                    errors={errors.name}
-                    handleChange={handleChange}
-                    field="name"
-                  />
-                  <TextInputField
-                    label="Cost"
-                    value={values.cost}
-                    errors={errors.cost}
-                    handleChange={handleChange}
-                    field="cost"
-                    keyboardType="numeric"
-                  />
-                  <DropDownPicker
-                    open={dropdownVIsible}
-                    value={value}
-                    items={[
-                      {label: 'Active', value: 'active'},
-                      {label: 'Inactive', value: 'inactive'},
-                    ]}
-                    setOpen={setDropdownVisible}
-                    setValue={setValue}
-                  />
-                  <ActionButton
-                    text="Update Restaurant"
-                    onPress={handleSubmit}
-                  />
-                </>
-              )}
-            </Formik>
-          </View>
-        </Modal>
-      </SafeAreaView>
+      <FlatList
+        data={restaurants}
+        keyExtractor={restaurant => restaurant}
+        renderItem={restaurant => (
+          <AdminField
+            id={restaurant.item}
+            title={restaurantsById[restaurant.item].name}
+            description={`Cost: ${restaurantsById[restaurant.item].cost} lei`}
+            icon="food"
+            onDelete={() => dispatch(deleteRestaurant({id: restaurant.item}))}
+            onEdit={() =>
+              navigation.navigate('RestaurantDetailsScreen', {
+                restaurantId: restaurant.item,
+              })
+            }
+          />
+        )}
+        onRefresh={onRefresh}
+        refreshing={isFetching}
+      />
     </HideKeyboard>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  title: {
-    fontSize: 24,
-    marginBottom: 10,
-    textAlign: 'center',
-  },
-});
 
 export default ManageRestaurantsScreen;
