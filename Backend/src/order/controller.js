d7b2f9d08cc66f8d26c38c5bd83b6caafb653793
Ovidiu Firescu <ovidiufirescu@gmail.com>
2021-07-20 14:50:26 +0300

    implemented create and delete controller for order

--- Backend/src/order/controller.js
+++ Backend/src/order/controller.js
@@ -1,12 +1,35 @@
+const NotFoundError = require('../errors/notFoundError');
 const Order = require('./model');
 
 const getOrders = async (req, res, next) => {};
 
-const createOrder = async (req, res, next) => {};
+const createOrder = async (req, res, next) => {
+  try {
+    let order = new Order(req.body);
+    order = await order.save();
+
+    res.sendStatus(201);
+  } catch (error) {
+    return next(error);
+  }
+};
 
 const updateOrder = async (req, res, next) => {};
 
-const deleteOrder = async (req, res, next) => {};
+const deleteOrder = async (req, res, next) => {
+  try {
+    const order = await Order.findOne({ _id: req.params._orderId });
+
+    if (!order) {
+      return next(new NotFoundError('Order not found'));
+    }
+    await order.delete();
+
+    res.status(204).send();
+  } catch (error) {
+    return next(error);
+  }
+};
 
 module.exports = {
   getOrders,
--- Backend/src/order/routes.js
+++ Backend/src/order/routes.js
@@ -6,7 +6,7 @@ const orderController = require('./controller');
 
 router.get('/', userAuthValidation, orderController.getOrders);
 router.post('/', userAuthValidation, orderController.createOrder);
-router.put('/', userAuthValidation, orderController.updateOrder);
-router.delete('/', userAuthValidation, orderController.deleteOrder);
+router.put('/:orderId', userAuthValidation, orderController.updateOrder);
+router.delete('/:_orderId', userAuthValidation, orderController.deleteOrder);
 
 module.exports = router;
