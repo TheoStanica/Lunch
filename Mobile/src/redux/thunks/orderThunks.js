e6dc07e8886fff5fec568415e07b78f3897ee32b
TheoStanica <theodor.stanica@gmail.com>
2021-07-29 17:19:54 +0300

    created thunks to get order and update order

--- Mobile/src/redux/thunks/orderThunks.js
+++ Mobile/src/redux/thunks/orderThunks.js
@@ -1,4 +1,8 @@
-import {orderCreateRequest} from './httpRequests';
+import {
+  orderCreateRequest,
+  orderGetRequest,
+  orderUpdateRequest,
+} from './httpRequests';
 import {handleError} from './errorThunks';
 
 export const createOrder =
@@ -6,6 +10,37 @@ export const createOrder =
   async dispatch => {
     try {
       await orderCreateRequest({menuId, userId, type, menuOptions});
+      if (typeof callback == 'function') callback();
+    } catch (error) {
+      dispatch(handleError(error));
+    }
+  };
+
+export const getOrder =
+  ({filter}, callback) =>
+  async dispatch => {
+    try {
+      const res = await orderGetRequest({filter});
+      if (typeof callback == 'function') callback(res.data);
+    } catch (error) {
+      dispatch(handleError(error));
+      if (typeof callback == 'function') callback(null);
+    }
+  };
+
+export const updateOrder =
+  ({orderId, type, status, userId, menuId, menuOptions}, callback) =>
+  async dispatch => {
+    try {
+      await orderUpdateRequest({
+        orderId,
+        type,
+        status,
+        userId,
+        menuId,
+        menuOptions,
+      });
+
       if (typeof callback == 'function') callback();
     } catch (error) {
       dispatch(handleError(error));
