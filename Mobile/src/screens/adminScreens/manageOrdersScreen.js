993a74b625cc9cf628daca1f3448f8766f2d5a7d
Ovidiu Firescu <ovidiufirescu@gmail.com>
2021-08-02 14:45:35 +0300

    improved Manage Orders Screen

--- Mobile/src/screens/adminScreens/manageOrdersScreen.js
+++ Mobile/src/screens/adminScreens/manageOrdersScreen.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, {useCallback} from 'react';
+import {useSelector, useDispatch} from 'react-redux';
 import {StyleSheet, SafeAreaView, Text} from 'react-native';
+import {getOrder} from '../../redux/thunks/orderThunks';
+import {useFocusEffect} from '@react-navigation/native';
 
 const ManageOrdersScreen = ({navigation}) => {
+  const {orders, ordersById} = useSelector(state => state.ordersReducer);
+  const dispatch = useDispatch();
+
+  const onRefresh = () => {
+    dispatch(
+      getOrder({
+        filter: {},
+        privilege: 'admin',
+      }),
+    );
+  };
+  useFocusEffect(
+    useCallback(() => {
+      onRefresh();
+    }, []),
+  );
+
   return (
     <SafeAreaView style={styles.container}>
       <Text>Manage Orders Sceen</Text>
