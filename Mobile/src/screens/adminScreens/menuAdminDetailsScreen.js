6c2151cf6e48390c59ed8addba0a4c5561e44129
Ovidiu Firescu <ovidiufirescu@gmail.com>
2021-08-03 10:56:23 +0300

    improved Menu Admin Details Screen

--- Mobile/src/screens/adminScreens/menuAdminDetailsScreen.js
+++ Mobile/src/screens/adminScreens/menuAdminDetailsScreen.js
@@ -1,6 +1,6 @@
 import React, {useCallback} from 'react';
 import {useSelector, useDispatch} from 'react-redux';
-import {StyleSheet, SafeAreaView, Text} from 'react-native';
+import {StyleSheet, SafeAreaView, FlatList, Text} from 'react-native';
 import {getOrder} from '../../redux/thunks/orderThunks';
 import {useFocusEffect} from '@react-navigation/native';
 
@@ -9,6 +9,22 @@ const MenuAdminDetailsScreen = ({route, navigation}) => {
   const {menu} = route.params;
   const dispatch = useDispatch();
 
+  //console.log(menu);
+  //console.log(ordersById);
+
+  const generateSummary = () => {
+    const summary = {};
+
+    summary.totalOrders = orders.length;
+    summary.totalRestaurantOrders = orders.filter(
+      order => ordersById[order].type === 'restaurant',
+    ).length;
+    summary.totalTakeawayOrders =
+      summary.totalOrders - summary.totalRestaurantOrders;
+    console.log(summary);
+  };
+
+  generateSummary();
   const onRefresh = () => {
     dispatch(
       getOrder({
@@ -25,7 +41,24 @@ const MenuAdminDetailsScreen = ({route, navigation}) => {
 
   return (
     <SafeAreaView style={styles.container}>
+      <Text>
+        {menu.restaurantId.name} {menu.restaurantId.cost} {menu.createdAt}
+      </Text>
       <Text>Manage Orders Sceen</Text>
+      <FlatList
+        data={orders.filter(order => ordersById[order].type === 'takeaway')}
+        keyExtractor={order => order}
+        renderItem={order => (
+          <FlatList
+            data={ordersById[order.item].menuOptions}
+            keyExtractor={option => option}
+            renderItem={option => console.log(option)}
+          />
+        )}
+        showsVerticalScrollIndicator={false}
+      />
+      <Text>Manage Orders Sceen</Text>
+      <Text>Summary</Text>
     </SafeAreaView>
   );
 };
