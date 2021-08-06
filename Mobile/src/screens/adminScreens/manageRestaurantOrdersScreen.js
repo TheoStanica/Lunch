5aabe63f0d6fa96db57a771d76e87dd6359d09f0
Ovidiu Firescu <ovidiufirescu@gmail.com>
2021-08-06 14:25:50 +0300

    created empty screen Manage Restaurant Orders Screen

---
+++ Mobile/src/screens/adminScreens/manageRestaurantOrdersScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+
+const ManageRestaurantOrdersScreen = () => {
+  return (
+    <View>
+      <Text>Manage Restaurant Orders Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default ManageRestaurantOrdersScreen;
