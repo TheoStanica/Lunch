42b6b12927ae0ae93d965ca42e8b33eb3d657f22
Ovidiu Firescu <ovidiufirescu@gmail.com>
2021-08-09 12:09:30 +0300

    improved Manage Orders Screen

--- Mobile/src/screens/adminScreens/manageOrdersScreen.js
+++ Mobile/src/screens/adminScreens/manageOrdersScreen.js
@@ -1,32 +1,36 @@
 import React, {useState, useEffect} from 'react';
 import {useSelector, useDispatch} from 'react-redux';
-import {StyleSheet, SafeAreaView} from 'react-native';
+import {StyleSheet, SafeAreaView, View, Text} from 'react-native';
 import {getOrder} from '../../redux/thunks/orderThunks';
 import {Title, Divider} from 'react-native-paper';
-import Statistics from '../../components/statistics';
+import {getRestaurants} from '../../redux/thunks/restaurantThunks';
+import {getAllUsers} from '../../redux/thunks/userThunks';
 import DateTimePicker from '../../components/timePicker';
+import DropDownPicker from 'react-native-dropdown-picker';
 import moment from 'moment';
 
 const ManageOrdersScreen = ({navigation}) => {
   const {orders, ordersById} = useSelector(state => state.ordersReducer);
+  const {restaurants, restaurantsById} = useSelector(
+    state => state.restaurantReducer,
+  );
+  const {allUsers, allUsersById} = useSelector(state => state.allUsersReducer);
   const [orderStart, setOrderStart] = useState(
     moment(Date.now()).format('DD-MM-YYYY'),
   );
   const [orderEnd, setOrderEnd] = useState(
     moment(Date.now()).format('DD-MM-YYYY'),
   );
+  const [openDropDown, setOpenDropDown] = useState(false);
+  const [openSecondDropDown, setOpenSecondDropDown] = useState(false);
+  const [selectedRestaurant, setSelectedRestaurant] = useState({});
+  const [selectedUser, setSelectedUser] = useState({});
   const [statistics, setStatistics] = useState({});
   const dispatch = useDispatch();
 
   const generateStatistics = () => {
     let restaurants = {},
-      statistics = {
-        totalOrders: 0,
-        totalTakeawayOrders: 0,
-        totalRestaurantOrders: 0,
-        totalTakeawayCost: 0,
-        totalCost: 0,
-      };
+      statistics = {};
 
     Object.values(ordersById).forEach(order => {
       const restaurant = order.menuId.restaurantId;
@@ -51,20 +55,44 @@ const ManageOrdersScreen = ({navigation}) => {
       }
     });
 
-    Object.values(restaurants).forEach(restaurant => {
-      restaurant.totalRestaurantOrders =
-        restaurant.totalOrders - restaurant.totalTakeawayOrders;
-      statistics.totalOrders += restaurant.totalOrders;
-      statistics.totalCost += restaurant.totalCost;
-      statistics.totalTakeawayOrders += restaurant.totalTakeawayOrders;
-      statistics.totalRestaurantOrders += restaurant.totalRestaurantOrders;
-      statistics.totalTakeawayCost += restaurant.totalTakeawayCost;
-    });
-
     statistics.restaurants = restaurants;
 
     return statistics;
   };
+
+  const generateRestaurantItems = () => {
+    const items = [];
+
+    items.push({label: 'All restaurants', value: {}});
+    restaurants.forEach(restaurantId => {
+      if (restaurantsById[restaurantId].status === 'active')
+        items.push({
+          label: restaurantsById[restaurantId].name,
+          value: restaurantsById[restaurantId].id,
+        });
+    });
+
+    return items;
+  };
+
+  const generateUsersItems = () => {
+    const items = [];
+    items.push({label: 'All users', value: {}});
+    allUsers.forEach(userId => {
+      if (allUsersById[userId].status === 'active')
+        items.push({
+          label: allUsersById[userId].fullname,
+          value: allUsersById[userId].id,
+        });
+    });
+
+    return items;
+  };
+
+  useEffect(() => {
+    dispatch(getRestaurants());
+    dispatch(getAllUsers());
+  }, []);
 
   useEffect(() => {
     dispatch(
@@ -106,15 +134,42 @@ const ManageOrdersScreen = ({navigation}) => {
         onConfirm={date => setOrderEnd(moment(date).format('DD-MM-YYYY'))}
         mode="date"
       />
+      <SafeAreaView style={styles.restaurantContainer(2000)}>
+        <Text>Restaurant </Text>
+        <DropDownPicker
+          open={openDropDown}
+          value={selectedRestaurant}
+          setValue={setSelectedRestaurant}
+          setOpen={setOpenDropDown}
+          items={generateRestaurantItems()}
+          style={styles.dropDownPicker}
+          dropDownContainerStyle={styles.dropDownContainerStyle}
+          placeholder="All restaurants"
+          style={styles.dropdownStyle}
+          selectedItemContainerStyle={{backgroundColor: '#4A6572'}}
+          selectedItemLabelStyle={styles.selectedItemLabel}
+          dropDownContainerStyle={styles.dropdownContainer}
+        />
+      </SafeAreaView>
+      <SafeAreaView style={styles.restaurantContainer(1000)}>
+        <Text>User </Text>
+        <DropDownPicker
+          open={openSecondDropDown}
+          value={selectedUser}
+          setValue={setSelectedUser}
+          setOpen={setOpenSecondDropDown}
+          items={generateUsersItems()}
+          style={styles.dropDownPicker}
+          dropDownContainerStyle={styles.dropDownContainerStyle}
+          placeholder="All users"
+          style={styles.dropdownStyle}
+          selectedItemContainerStyle={{backgroundColor: '#4A6572'}}
+          selectedItemLabelStyle={styles.selectedItemLabel}
+          dropDownContainerStyle={styles.dropdownContainer}
+        />
+      </SafeAreaView>
       <Divider style={styles.divider} />
       <Title style={styles.title}>Statistics</Title>
-      <Statistics
-        totalOrders={statistics.totalOrders}
-        totalRestaurantOrders={statistics.totalRestaurantOrders}
-        totalTakeawayOrders={statistics.totalTakeawayOrders}
-        totalTakeawayCost={statistics.totalTakeawayCost}
-        totalCost={statistics.totalCost}
-      />
     </SafeAreaView>
   );
 };
@@ -131,6 +186,27 @@ const styles = StyleSheet.create({
   title: {
     alignSelf: 'center',
   },
+  dropdownStyle: {
+    backgroundColor: 'transparent',
+    borderColor: '#0007',
+    borderWidth: 0.3,
+  },
+  selectedItemLabel: {
+    color: 'white',
+  },
+  dropdownContainer: {
+    backgroundColor: '#FFF1CA',
+    borderColor: '#0007',
+    borderWidth: 0.3,
+  },
+  restaurantContainer: zIndex => ({
+    marginHorizontal: 15,
+    marginVertical: 5,
+    zIndex: zIndex,
+    flexDirection: 'row',
+    alignItems: 'center',
+    flexWrap: 'wrap',
+  }),
 });
 
 export default ManageOrdersScreen;
