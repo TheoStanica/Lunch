88d3726a1468230c367fd4f590125355384341a5
TheoStanica <theodor.stanica@gmail.com>
2021-08-13 17:12:34 +0300

    added device routes

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -6,6 +6,7 @@ const restaurantRoutes = require('./restaurant/routes');
 const orderRoutes = require('./order/routes');
 const menuRoutes = require('./menu/routes');
 const notificationRoutes = require('./notification/routes');
+const deviceRoutes = require('./device/routes');
 
 apiRouter.get('/deeplink/:_path*', (req, res) => {
   res.redirect(`lunchapp://${req.params._path}${req.params[0]}`);
@@ -16,5 +17,6 @@ apiRouter.use('/restaurant', restaurantRoutes);
 apiRouter.use('/order', orderRoutes);
 apiRouter.use('/menu', menuRoutes);
 apiRouter.use('/notification', notificationRoutes);
+apiRouter.use('/device', deviceRoutes);
 
 module.exports = apiRouter;
