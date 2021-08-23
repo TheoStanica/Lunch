02a071730d5a61b025bb3376949c25361e92a919
Ovidiu Firescu <ovidiufirescu@gmail.com>
2021-08-23 15:14:54 +0300

    installed react-native-picker-select and @react-native-picker/picker

--- Mobile/src/redux/thunks/errorThunks.js
+++ Mobile/src/redux/thunks/errorThunks.js
@@ -4,6 +4,7 @@ export const handleError = error => async dispatch => {
   if (error?.response?.data?.errors) {
     dispatch(setErrors(error.response.data.errors));
   } else {
+    console.log(error);
     dispatch(setErrors([{message: 'Something went wrong'}]));
   }
 };
